ccd32aa2e2c569b701636ae5ed8337b121f6ce74
Mohammed Siddiq <sharqion1011@gmail.com>
2023-12-30 01:54:08 +0530

    css and html bruh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,30 +21,59 @@ function App() {
     fetchData();
   }, []);
 
+
   return (
-    <div className="App">
-      <h1>Access Logs</h1>
+    <div className="container">
+      <h2 style={{ color: '#00FFEF' }}>Access Logs </h2>
       <button onClick={fetchData}>Fetch Logs</button>
-      <table>
-        <thead>
-          <tr>
-            <th>IPaddress</th>
-            <th>Date and Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
-              <td>{item.ip_address}</td>
-              <td>{item.date_time}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+
+      <ul className="responsive-table">
+      
+        <li className="table-header">
+          <div className="col col-1">IP Address</div>
+          <div className="col col-2">Date and Time</div>
+        </li>
+        {data.map((item, index) => (
+          <li className="table-row" key={index}>
+            <div className="col col-1" data-label="IP Address">
+              {item.ip_address}
+            </div>
+            <div className="col col-2" data-label="Date and Time">
+              {item.date_time}
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
+
 export default App;
 
+  /*un comment for OG bruhhhhh!
+  return (
+    <div className="App">
+      <h1>Access Logs</h1>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>IPaddress</th>
+              <th>DateandTime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.ip_address}</td>
+                <td>{item.date_time}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}*/
 
