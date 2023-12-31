46eba78f1534f6dd35c92f79e8929059f99f8432
noobed_max <146673372+noobed-max@users.noreply.github.com>
2023-12-31 18:57:18 +0530

    Update main.py with Month to number converter

--- backend/main.py
+++ backend/main.py
@@ -17,6 +17,22 @@ app.add_middleware(
 )
 LOG_FILE_PATH = config['fastapi']['log_file_path']
 
+MONTH_MAP = {
+    "Jan": "1",
+    "Feb": "2",
+    "Mar": "3",
+    "Apr": "4",
+    "May": "5",
+    "Jun": "6",
+    "Jul": "7",
+    "Aug": "8",
+    "Sep": "9",
+    "Oct": "10",
+    "Nov": "11",
+    "Dec": "12",
+}
+
+
 @app.get("/logs")
 def get_access_logs():
     try:
@@ -25,17 +41,32 @@ def get_access_logs():
 
         result = []
         for log in access_logs:
-            # Extract IP address and date-time from log entries
+            # Extract IP address, date, and time from log entries
             ip_address = re.search(r'^([\d.]+)', log)
-            date_time = re.search(r'\[([^:]+:[^\]]+)\]', log)
+            date_time = re.search(r'\[([^:]+):([^\]]+)\s[+\-\d]+\]', log)  # Updated regex
 
             if ip_address and date_time:
+                date = date_time.group(1)
+                time = date_time.group(2)
+
+                # Add 05:30 to the time
+                time_obj = datetime.strptime(time, '%H:%M:%S')
+                time_obj += timedelta(hours=5, minutes=30)
+                time = time_obj.strftime('%H:%M:%S')
+
+                # Update date format
+                day, month, year = date.split('/')
+                month = MONTH_MAP[month]
+                updated_date = f"{day}/{month}/{year}"
+
                 result.append({
                     "ip_address": ip_address.group(1),
-                    "date_time": date_time.group(1)
+                    "date": updated_date,
+                    "time": time
                 })
 
         return JSONResponse(content=result)
+
 
     except FileNotFoundError:
         return JSONResponse(content={"error": "Access log file not found"}, status_code=404)
