139b2ed5bab668b2e6759f5130160e090d55197e
noobed_max <146673372+noobed-max@users.noreply.github.com>
2023-12-31 18:55:20 +0530

    udpates app.js with date filtration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import config from './config.json';
 function App() {
   const [data, setData] = useState([]);
   const [intervalId, setIntervalId] = useState(null);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
   const fetchData = () => {
     fetch(config.react.fastapi_url)
@@ -34,6 +36,20 @@ function App() {
     }
   };
 
+  const filterData = () => {
+    if (startDate && endDate && startDate > endDate) {
+      setData([]);
+    } else if (startDate || endDate) {
+      const start = startDate ? new Date(startDate.split('/').reverse().join('-')) : new Date(0);
+      const end = endDate ? new Date(endDate.split('/').reverse().join('-')) : new Date();
+      const filteredData = data.filter(item => {
+        const itemDate = new Date(item.date.split('/').reverse().join('-'));
+        return itemDate >= start && itemDate <= end;
+      });
+      setData(filteredData);
+    }
+  };
+
   useEffect(() => {
     return () => {
       if (intervalId) {
@@ -45,21 +61,28 @@ function App() {
   return (
     <div className="container">
       <h2 style={{ color: '#00FFEF' }}>Access Logs </h2>
+      <input type="date" value={startDate} onChange={e => setStartDate(e.target.value)} />
+      <input type="date" value={endDate} onChange={e => setEndDate(e.target.value)} />
+      <button onClick={filterData}>Filter</button>
       <button onClick={startFetching}>Start Fetching</button>
       <button onClick={stopFetching}>Stop Fetching</button>
 
       <ul className="responsive-table">
         <li className="table-header">
           <div className="col col-1">IP Address</div>
-          <div className="col col-2">Date and Time</div>
+          <div className="col col-2">Date</div>
+          <div className="col col-3">Time</div>
         </li>
         {data.map((item, index) => (
           <li className="table-row" key={index}>
             <div className="col col-1" data-label="IP Address">
               {item.ip_address}
             </div>
-            <div className="col col-2" data-label="Date and Time">
-              {item.date_time}
+            <div className="col col-2" data-label="Date">
+              {item.date}
+            </div>
+            <div className="col col-3" data-label="Time">
+              {item.time}
             </div>
           </li>
         ))}
